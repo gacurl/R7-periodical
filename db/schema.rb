80a1597974409a6a566c08747069629880803b06
gacurl <greg.a.curl@gmail.com>
2023-04-25 06:03:27 -0400

    create subscription and add a subscriber

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_094634) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_094940) do
   create_table "magazines", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_094634) do
 
   create_table "subscribers", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "months"
+    t.integer "subscriber_id"
+    t.integer "magazine_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
